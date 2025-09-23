674d6b3383df2e293ab7a0a472f18700bee046f2
Endraagustino22 <endraagustino2018@gmail.com>
2025-09-23 17:25:06 +0700

    add : menambahkan model dan migration pengumuman

---
+++ bapperida/app/Models/Pengumuman.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Pengumuman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pengumuman';
+
+    protected $fillable = [
+        'judul', 'isi', 'tanggal', 'file_pengumuman'
+    ];
+}
---
+++ bapperida/database/migrations/2025_09_23_102158_create_pengumumen_table.php
@@ -0,0 +1,23 @@
+<?php
+
+// database/migrations/2025_09_23_000002_create_pengumuman_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('pengumuman', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('isi');
+            $table->date('tanggal');
+            $table->string('file_pengumuman')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('pengumuman');
+    }
+};
